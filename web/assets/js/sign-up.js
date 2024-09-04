c92c055db5fd145dee0a9355a4b1e3786425bf1b
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-04 14:31:22 +0530

    "Updated project files: changed open files in private.xml, updated Java version in project.properties, modified SignUp.java and sign-up.js files, and updated sign-up.html file."

--- web/assets/js/sign-up.js
+++ web/assets/js/sign-up.js
@@ -1,18 +1,17 @@
 async function SignUp() {
+   const user_dto = {
+       firstName:document.getElementById("first_name").value,
+       lastName:document.getElementById("last_name").value,
+       email:document.getElementById("email").value,
+       password:document.getElementById("password").value,
+   }
     
-    const user_dto = {
-        firstName:document.getElementById("first_name").value,
-        lastName:document.getElementById("last_name").value,
-        email:document.getElementById("email").value,
-        password:document.getElementById("password").value,
-    }
-    
-  const response = await fetch("SignUp",{method:"POST",body:JSON.stringify(user_dto),headers:{"Content-Type":"application/json"}});
-    
-    if (response.ok) {
-        const json = await response.json;
-        console.log(json);
-    } else {
-        console.log("Error");
-    }
+ const response = await fetch("SignUp",{method:"POST",body:JSON.stringify(user_dto),headers:{"Content-Type":"application/json"}});
+   
+   if (response.ok) {
+       const json = await response.json;
+       console.log(json);
+   } else {
+       console.log("Error");
+   }
 }
