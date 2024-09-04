bd6f7e0cd086c0fc966787153dc558adf77ff7d5
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-04 16:39:30 +0530

    "Updated Verify_Account servlet to handle verification code validation and update user verification status"

--- src/java/controller/Verify_Account.java
+++ src/java/controller/Verify_Account.java
@@ -21,30 +21,47 @@ import org.hibernate.criterion.Restrictions;
 
 @WebServlet(name = "Verify_Account", urlPatterns = {"/Verify_Account"})
 public class Verify_Account extends HttpServlet {
-
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        
         System.out.println("done");
-
+        
         Response_DTO response_DTO = new Response_DTO();
-
+        
         Gson gson = new Gson();
-
+        
         JsonObject dto = gson.fromJson(request.getReader(), JsonObject.class);
         
         String verification = dto.get("verification").getAsString();
         
-        if (request.getSession().getAttribute("email")!=null) {
-            response_DTO.setContent("");
-        }else{
+        if (request.getSession().getAttribute("email") != null) {
+            String email = request.getSession().getAttribute("email").toString();
             
+            Session session = HibernateUtil.getSessionFactory().openSession();
             
+            Criteria criteria1 = session.createCriteria(User.class);
+            criteria1.add(Restrictions.eq("emial", email));
+            criteria1.add(Restrictions.eq("verification", verification));
             
+            if (!criteria1.list().isEmpty()) {
+                User user = (User) criteria1.list().get(0);
+                user.setVerification("Verified");
+                session.update(user);
+                session.beginTransaction().commit();
+                request.removeAttribute("email");
+                response_DTO.setSuccess(true);
+                response_DTO.setContent("success");
+            } else {
+                response_DTO.setContent("Invalid verification code!");
+            }
+            
+        } else {
+            response_DTO.setContent("Verification unavailbe! Please Sign In!");
         }
-
+        
         response.setContentType("application/json");
         response.getWriter().write(gson.toJson(response_DTO));
-
+        
     }
-
+    
 }
