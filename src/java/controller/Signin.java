97bd8ebfde57e04dd6eb600a8bcb1751e49c610b
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-04 15:03:20 +0530

    "Added imports, implemented doPost method in Signin.java to handle user signin and registration with email verification"

--- src/java/controller/Signin.java
+++ src/java/controller/Signin.java
@@ -1,5 +1,10 @@
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.Response_DTO;
+import dto.User_DTO;
+import entity.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -7,10 +12,70 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import model.Mail;
+import model.Validations;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @WebServlet(name = "Signin", urlPatterns = {"/Signin"})
 public class Signin extends HttpServlet {
 
-    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("done");
+
+        Response_DTO response_DTO = new Response_DTO();
+
+        Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+
+        User_DTO user = gson.fromJson(request.getReader(), User_DTO.class);
+
+
+        if (user.getEmail().isBlank()) {
+            response_DTO.setContent("Please enter your email!");
+        } else if (!Validations.isEmailValid(user.getEmail())) {
+            response_DTO.setContent("Please enter valid email!");
+        } else if (user.getPassword().isBlank()) {
+            response_DTO.setContent("Please enter your password!");
+        } else if (!Validations.isPasswordValid(user.getPassword())) {
+            response_DTO.setContent("Please enter valid password!");
+        } else {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+
+            Criteria criteria = session.createCriteria(User.class).add(Restrictions.eq("email", user.getEmail()));
+
+            if (!criteria.list().isEmpty()) {
+                response_DTO.setContent("User with this Email already exists");
+            } else {
+                User userEntity = new User();
+                int code = (int) (Math.random() * 1000000);
+
+                userEntity.setFirst_name(user.getFirst_name());
+                userEntity.setLast_name(user.getLast_name());
+                userEntity.setPassword(user.getPassword());
+                userEntity.setVerification(String.valueOf(code));
+
+                Thread t = new Thread();
+                Mail.sendMail("sdilusha34@gmail.com", "Smart Trade Verification", "<h1 style=\"color:red\">" + userEntity.getVerification() + "</h1>");
+                t.start();
+
+                session.save(userEntity);
+                session.beginTransaction().commit();
+                response_DTO.setSuccess(true);
+                response_DTO.setContent("Registration Complete");
+
+            }
+
+            session.close();
+
+        }
+
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(response_DTO));
+
+    }
 
 }
