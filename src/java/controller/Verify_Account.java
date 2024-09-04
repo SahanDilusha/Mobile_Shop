818856002f9cab6d3b45e2a347e2b1eda4aea054
SahanDilusha <sahancodelygroup@gmail.com>
2024-09-04 16:25:15 +0530

    "Updated Verify_Account.java and verify-account.js to modify verification code handling and JSON data exchange"

--- src/java/controller/Verify_Account.java
+++ src/java/controller/Verify_Account.java
@@ -1,5 +1,11 @@
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import dto.Response_DTO;
+import dto.User_DTO;
+import entity.User;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -7,15 +13,38 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import model.Validations;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 @WebServlet(name = "Verify_Account", urlPatterns = {"/Verify_Account"})
 public class Verify_Account extends HttpServlet {
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    
+        System.out.println("done");
+
+        Response_DTO response_DTO = new Response_DTO();
+
+        Gson gson = new Gson();
+
+        JsonObject dto = gson.fromJson(request.getReader(), JsonObject.class);
+        
+        String verification = dto.get("verification").getAsString();
+        
+        if (request.getSession().getAttribute("email")!=null) {
+            response_DTO.setContent("");
+        }else{
+            
+            
+            
+        }
+
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(response_DTO));
+
+    }
 
 }
--- web/assets/js/verify-account.js
+++ web/assets/js/verify-account.js
@@ -1,12 +1,12 @@
 async  function VerifyAccount() {
     
-     const user_dto = {
-        email: document.getElementById("verificationCode").value
+     const dto = {
+        verification: document.getElementById("verificationCode").value
     }
 
     console.log(user_dto);
 
-    const response = await fetch("Verify_Account", {method: "POST", body: JSON.stringify(user_dto), headers: {"Content-Type": "application/json"}});
+    const response = await fetch("Verify_Account", {method: "POST", body: JSON.stringify(dto), headers: {"Content-Type": "application/json"}});
 
     if (response.ok) {
         const json = await response.json;
